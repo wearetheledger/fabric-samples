fd5b43c146643f563173e35767f9ac03ec33dc81
andriesvh <drikkesband@hotmail.com>
2018-01-12 10:58:43 +0100

    aws

--- basic-network/start.sh
+++ basic-network/start.sh
@@ -9,7 +9,8 @@ set -ev
 
 # don't rewrite paths for Windows Git Bash users
 export MSYS_NO_PATHCONV=1
-export HOST_ORDERER=localhost
+export HOST_ORDERER=ec2-34-251-33-15.eu-west-1.compute.amazonaws.com
+export PORT_ORDERER=7050
 # export HOST_ORDERER=ec2-34-251-33-15.eu-west-1.compute.amazonaws.com
 
 # docker-compose -f docker-compose.yml down
@@ -24,6 +25,6 @@ export FABRIC_START_TIMEOUT=10
 sleep ${FABRIC_START_TIMEOUT}
 
 # Create the channel
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o ${HOST_ORDERER}:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o ${HOST_ORDERER}:${PORT_ORDERER} -c mychannel -f /etc/hyperledger/configtx/channel.tx
 # Join peer0.org1.example.com to the channel.
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b mychannel.block
