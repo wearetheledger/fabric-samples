b967d6db4cefb71639a46c4743672669325bce9b
Jeroen De Prest <jeroen.deprest@hotmail.com>
2018-01-16 10:05:32 +0100

    Extracted peer

---
+++ fabcar/startPeer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Exit on first error
+set -e
+set -x
+
+# don't rewrite paths for Windows Git Bash users
+export MSYS_NO_PATHCONV=1
+
+starttime=$(date +%s)
+
+# launch network; create channel and join peer to channel
+cd ../basic-network
+./start-peer.sh
