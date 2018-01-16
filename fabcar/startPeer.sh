a74b1780a190aa6d28ea2cc06bab6d2683f582ab
Jeroen De Prest <jeroen.deprest@hotmail.com>
2018-01-16 10:22:32 +0100

    first test

--- fabcar/startPeer.sh
+++ fabcar/startPeer.sh
@@ -16,3 +16,14 @@ starttime=$(date +%s)
 # launch network; create channel and join peer to channel
 cd ../basic-network
 ./start-peer.sh
+
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n fabcar -v 1.0 -p github.com/fabcar
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o ${HOST_ORDERER}:7050 -C mychannel -n fabcar -v 1.0 -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
+sleep 10
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o ${HOST_ORDERER}:7050 -C mychannel -n fabcar -c '{"function":"initLedger","Args":[""]}'
+
+printf "\nTotal setup execution time : $(($(date +%s) - starttime)) secs ...\n\n\n"
+printf "Start by installing required packages run 'npm install'\n"
+printf "Then run 'node enrollAdmin.js', then 'node registerUser'\n\n"
+printf "The 'node invoke.js' will fail until it has been updated with valid arguments\n"
+printf "The 'node query.js' may be run at anytime once the user has been registered\n\n"
