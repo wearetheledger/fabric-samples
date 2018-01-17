e4d51188d8925383d429fa4f504bd844aee1b27a
Jeroen De Prest <jeroen.deprest@hotmail.com>
2018-01-17 14:41:36 +0100

    Trying something

--- fabcar/CreateJoinChannel.sh
+++ fabcar/CreateJoinChannel.sh
@@ -18,4 +18,4 @@ docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/g
 # Join peer0.org1.example.com to the channel.
 docker exec -e "CORE_PEER_ID=peer0.org1.example.com" -e "CORE_PEER_ADDRESS=ec2-34-241-181-15.eu-west-1.compute.amazonaws.com:7051" -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer channel join -b mychannel.block
 
-docker exec -e "CORE_PEER_ID=peer0.org2.example.com" -e "CORE_PEER_ADDRESS=ec2-54-229-222-164.eu-west-1.compute.amazonaws.com:7051" -e "CORE_PEER_LOCALMSPID=Org2MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/fabric/msp/org2.example.com/peers/peer0.org2.example.com/msp" cli peer channel join -b mychannel.block
+docker exec -e "CORE_PEER_ID=peer0.org2.example.com" -e "CORE_PEER_ADDRESS=ec2-54-229-222-164.eu-west-1.compute.amazonaws.com:7051" -e "CORE_PEER_LOCALMSPID=Org2MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp" cli peer channel join -b mychannel.block
--- fabcar/startPeerOrg1.sh
+++ fabcar/startPeerOrg1.sh
@@ -17,3 +17,19 @@ export PORT_ORDERER=7050
 # launch network; create channel and join peer to channel
 cd ../basic-network
 ./start-peer-org1.sh
+
+# # Create the channel 
+# docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o ${HOST_ORDERER}:${PORT_ORDERER} -c mychannel -f /etc/hyperledger/configtx/channel.tx 
+# # Join peer0.org1.example.com to the channel. 
+# docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b mychannel.block 
+ 
+# docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n fabcar -v 1.0 -p github.com/fabcar 
+# docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o ${HOST_ORDERER}:${PORT_ORDERER} -C mychannel -n fabcar -v 1.0 -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')" 
+# sleep 10 
+# docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o ${HOST_ORDERER}:${PORT_ORDERER} -C mychannel -n fabcar -c '{"function":"initLedger","Args":[""]}' 
+ 
+# printf "\nTotal setup execution time : $(($(date +%s) - starttime)) secs ...\n\n\n" 
+# printf "Start by installing required packages run 'npm install'\n" 
+# printf "Then run 'node enrollAdmin.js', then 'node registerUser'\n\n" 
+# printf "The 'node invoke.js' will fail until it has been updated with valid arguments\n" 
+# printf "The 'node query.js' may be run at anytime once the user has been registered\n\n"
